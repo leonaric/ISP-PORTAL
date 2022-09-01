e171b5537c1d5f3535c01049898c1d8f05df32e5
leonaric <112154922+leonaric@users.noreply.github.com>
2022-09-01 04:17:36 -0700

    first project
    IPT PROJECT FOR ORGANISATION

---
+++ admin/post.js
@@ -0,0 +1,5 @@
+document.getElementById('six').innerHTML=localStorage.getItem('one');
+document.getElementById('seven').innerHTML=localStorage.getItem('two');
+document.getElementById('eight').innerHTML=localStorage.getItem('three');
+document.getElementById('nine').innerHTML=localStorage.getItem('four');
+document.getElementById('ten').innerHTML=localStorage.getItem('five');
